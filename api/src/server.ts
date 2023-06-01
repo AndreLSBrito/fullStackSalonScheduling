5723a88223c48da98c613bc06ed3d8c2267a6be3
Tiotedd <andrelsbrito2022@gmail.com>
2023-06-01 01:24:21 -0300

    first commit

---
+++ api/src/server.ts
@@ -0,0 +1,28 @@
+import express, { Application, NextFunction, Request, Response } from 'express'
+import { UsersRoutes } from './routes/users.routes'
+import { SchedulesRoutes } from './routes/schedules.Routes'
+
+const  app:Application = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true})) // /old?text /ola%20mundo
+
+const usersRoutes = new UsersRoutes().getRoutes()
+const schedulesRoutes = new SchedulesRoutes().getRoutes()
+
+app.use('/users',usersRoutes)
+app.use('/schedules',schedulesRoutes)
+
+app.use((err:Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error){
+    return response.status(400).json({
+      message: err.message
+    })
+  }
+  return response.status(500).json({
+    message: 'Internal Server Error'
+  })
+})
+
+app.listen(3000, ()=> console.log('Server is runing on port 3000'))
+
