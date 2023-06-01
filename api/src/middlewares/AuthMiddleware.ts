af881f45e264c07e06fa02193b694e7785ee4b6b
Tiotedd <andrelsbrito2022@gmail.com>
2023-06-01 01:35:19 -0300

    created the middleware for athentication

---
+++ api/src/middlewares/AuthMiddleware.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload{
+  sub:string
+}
+
+export class AuthMiddleware{
+  auth(request:Request, response:Response, next:NextFunction){
+    const authHeader = request.headers.authorization
+    if(!authHeader){
+      return response.status(401).json({
+        code: 'token.missing',
+        message: 'Token missing'
+      })
+    }
+    // Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuZHJlQHRlc3RlLmNvbSIsImlhdCI6MTY4NTU4MjI3NCwiZXhwIjoxNjg1NTgzMTc0LCJzdWIiOiI2MmNiMzRlMC00YWNlLTQyY2EtYWUxYy04MDRkZWQwZTBiOTEifQ.EASywUVOgo0PkqST4Qb5V-0wmJHeJrQgG4VpSocG0b4
+    const [,token] = authHeader.split(' ');
+    let secretKey:string | undefined = process.env.ACCESS_KEY_TOKEN
+
+    if(!secretKey){
+      throw new Error('There is no token key')
+    }
+
+    try {
+      const {sub } = verify(token, secretKey) as IPayload
+      request.user_id = sub
+      return next()
+    } catch (error) {
+      return response.status(401).json({
+        code: 'token.expired',
+        message: 'Token Expired'
+      })
+    }
+
+  }
+}
