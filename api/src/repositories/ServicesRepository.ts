35e67f628088d70a81706392bc66e3de4843065d
Tiotedd <andrelsbrito2022@gmail.com>
2023-06-01 01:35:31 -0300

    created service repository

---
+++ api/src/repositories/ServicesRepository.ts
@@ -0,0 +1,21 @@
+import { prisma } from "../database/prisma";
+import { ICreate } from "../interfaces/SchedulesInterface";
+
+export class SchedulesRepository{
+ async create({name, phone, date}:ICreate){
+    const result = await prisma.schedule.create({
+      data: {
+        name,
+        phone,
+        date
+      }
+    })
+    return result
+  }
+  async find(date:Date){
+    const result = await prisma.schedule.findFirst({
+      where:{date}
+    })
+    return result
+  }
+}
