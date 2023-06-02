187f044a1eac07cad8e0aac8d1867537923ac5b8
Tiotedd <andrelsbrito2022@gmail.com>
2023-06-01 22:46:22 -0300

    added index and update controller

--- api/src/controllers/SchedulesController.ts
+++ api/src/controllers/SchedulesController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { SchedulesService } from "../services/SchedulesService";
+import { parseISO } from "date-fns";
 
 export class SchedulesController{
   private schedulesService: SchedulesService
@@ -8,12 +9,43 @@ export class SchedulesController{
   }
   async store(request: Request, response:Response, next:NextFunction){
     const { name, phone, date} = request.body
+    const {user_id} = request
+
     try {
-      const result = await this.schedulesService.create({name, phone, date})
+      const result = await this.schedulesService.create({
+        name,
+        phone,
+        date, 
+        user_id})
 
       return response.status(201).json(result)
     } catch (error) {
       next(error)
     }
   }
+  async index(request: Request, response:Response, next:NextFunction){
+    const {date} = request.query
+    const parseDate = date ? parseISO(date.toString()): new Date()
+    try {
+      const result = await this.schedulesService.index(parseDate)
+
+      return response.json(result)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async update(request: Request, response:Response, next:NextFunction){
+    const { id} = request.params
+    const { date} = request.body
+    const {user_id} = request
+
+    try {
+      const result = await this.schedulesService.update(id, date, user_id)
+
+      return response.json(result)
+    } catch (error) {
+      next(error)
+    }
+  }
+  delete(request: Request, response:Response, next:NextFunction){}
 }
