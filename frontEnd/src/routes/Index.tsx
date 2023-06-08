9592d90759f09eada74da5b72be83fe4ad260d4d
Tiotedd <andrelsbrito2022@gmail.com>
2023-06-08 13:03:35 -0300

    added new routes

--- frontEnd/src/routes/Index.tsx
+++ frontEnd/src/routes/Index.tsx
@@ -1,6 +1,10 @@
 import { Routes, Route} from 'react-router-dom'
 import { Login } from '../page/Login/Index'
 import { Register } from '../page/Register/Index'
+import { Dashboard } from '../page/Dashboard'
+import { Schedules } from '../page/Schedules/Index'
+import { PrivateRoute } from './PrivateRoute'
+import { EditProfile } from '../page/EditProfile/Index'
 
 export const RouteApp = () =>{
   return(
@@ -13,6 +17,29 @@ export const RouteApp = () =>{
       <Route
         path='/register'
         element={<Register/>}
+      />
+      <Route
+        path='/dashboard'
+        element={
+        <PrivateRoute>
+          <Dashboard/>
+        </PrivateRoute> }
+      />
+      <Route
+        path='/schedules'
+        element={
+          <PrivateRoute>
+            <Schedules/>
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path='/edit-profile'
+        element={
+          <PrivateRoute>
+            <EditProfile/>
+          </PrivateRoute>
+        }
       />
 
     </Routes>
